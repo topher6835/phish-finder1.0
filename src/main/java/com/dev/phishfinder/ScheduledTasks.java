6b402889a5714e9a3aa3e46fdec4e9d8a6317756
Chris <sears.topher@gmail.com>
2019-05-06 16:07:35 -0400

    log info evict chaches

--- src/main/java/com/dev/phishfinder/ScheduledTasks.java
+++ src/main/java/com/dev/phishfinder/ScheduledTasks.java
@@ -1,5 +1,7 @@
 package com.dev.phishfinder;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,7 @@ import com.dev.phishfinder.web.YearController;
 
 @Component
 public class ScheduledTasks {
+	private final Logger log = LoggerFactory.getLogger(ShowController.class);
 	
 	@Autowired
 	private YearController yearController;
@@ -21,6 +24,7 @@ public class ScheduledTasks {
 	@Scheduled(cron = "0 0 6 * * ?")
 	public void evictCache() {
 		System.out.println("Running scheduled cache flush...");
+		log.info("Running scheduled cache flush");
 		yearController.evictYears();
 		showController.evictYears();
 		songController.evictSongs();
